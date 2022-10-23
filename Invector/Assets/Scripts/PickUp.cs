1fa3c55ce3923abfd8e54585c90b13c2843b10d6
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2022-10-23 17:17:25 +0300

    Version1.0

---
+++ Invector/Assets/Scripts/PickUp.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+    public AudioSource coinmusic;
+    public int score;
+    public Text scoreText;
+    public GameObject winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: " + score;
+        winText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "coin")
+        {
+            Destroy(collision.gameObject);
+            score++;
+            scoreText.text = "Score: " + score;
+            coinmusic.Play();
+            
+        }
+
+        if (collision.gameObject.tag == "danger")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "finish")
+        {
+            winText.SetActive(true);
+        }
+    }
+
+
+}
